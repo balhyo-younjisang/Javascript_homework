280e4aaa3962e97f72dd61a1ed008473c6f76e5e
balhyo-younjisang <yjs12180825@gmail.com>
2024-08-30 20:01:36 +0900

    REFACTOR: Refactoring

--- scripts/room.js
+++ scripts/room.js
@@ -1,27 +1,36 @@
-let socket, universeName, roomId;
-
+/** ---------------- DOM 관련 변수 --------------------- */
 const leaveBtn = document.querySelector(".leaveRoom");
 const title = document.querySelector(".title");
 const players = document.querySelector(".players");
 const startBtn = document.querySelector(".start");
 
-let playerSpeed = 0.1;
-let jumpSpeed = 0.2;
-let gravity = 0.01;
-let isJumping = false;
-let moveForward = false, moveBackward = false, moveLeft = false, moveRight = false;
+/** ---------------- Socket 관련 변수 --------------------- */
+let socket, universeName, roomId;
+let playerId = null;
+let otherPlayers = {};
 
-let mouseSensitivity = 0.003; // Mouse sensitivitylet yaw = 0; // Horizontal rotationlet pitch = 0; // Vertical rotation
-
+/** ---------------- 게임 관련 변수 --------------------- */
+let scene, camera, renderer, player, floor;
+let playerSpeed = 0.1; // 기본 플레이어 이동 속도
+let jumpSpeed = 0.3; // 기본 플레이어 점프 속도
+let gravity = 0.01; // 플레이어에게 가해지는 중력의 크기 ( 점프 시 바닥으로 끌어당김 )
+let isJumping = false; // 플레이어가 점프 중인지 저장
+let moveForward = false, moveBackward = false, moveLeft = false, moveRight = false; // 키보드 입력 시 어디로 이동할지 저장
+let mouseSensitivity = 0.003; // 마우스 감도
 let yaw = 0;
 let pitch = 0;
 let lastMouseX = window.innerWidth / 2;
 let lastMouseY = window.innerHeight / 2; 
-let scene, camera, renderer, player;
-let floorY = -1;
 const pitchLimit = Math.PI / 36;
+let floorY = -1;
 
-function initThreeJS() {
+/** -------------------- 함수 선언부 -------------------------- */
+/***
+ * 게임 화면 초기화
+ */
+function init() {
+  document.body.innerHTML = '';
+
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(
     75,
@@ -29,23 +38,43 @@ function initThreeJS() {
     0.1,
     1000
   );
-
-  renderer = new THREE.WebGLRenderer();
+  renderer = new THREE.WebGLRenderer({
+    canvas: document.body.appendChild(document.createElement("canvas")),
+    antialias: false,
+    logarithmicDepthBuffer: true,
+  });
   renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
+  renderer.setClearColor(0x000000, 1);
+
+  const playerGeometry = new THREE.BoxGeometry(1, 1, 1);
+  const playerMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  player = new THREE.Mesh(playerGeometry, playerMaterial);
+  scene.add(player);
+
+  const floorGeometry = new THREE.PlaneGeometry(100, 100);
+  const floorMaterial = new THREE.MeshBasicMaterial({ color: 0xaaaaaa });
+  floor = new THREE.Mesh(floorGeometry, floorMaterial);
+  floor.rotation.x = -Math.PI / 2;
+  floor.position.y = floorY;
+  scene.add(floor);
+
+  camera.position.set(0, 0.9, 0);
+  player.position.set(0, 0, 0);
+  player.rotation.set(0, 0, 0);
+  player.add(camera);
+
+  animate();
 }
 
-let playerId = null;
-let otherPlayers = {};
-
-const changeRoomInit = (name) => {
-  document.title = `Room ${name} | GalaxyWar`;
-  title.textContent = `Welcome to ${name} Universe ...`;
-};
-
-const changeJoinedPlayer = (nowPlayers) => {
-  players.textContent = `Players : ${nowPlayers ? nowPlayers : 0}/6`;
-};
+/**
+ * 대기실 설정
+ * @param {*} data 
+ */
+function changeRoomData (data) {
+  document.title = `Room ${data.id} | GalaxyWar`;
+  title.textContent = `Welcome to ${data.id} Universe ...`;
+  players.textContent = `Players : ${data.players ? data.players : 0}/6`;
+}
 
 const leaveRoomHandler = (socket) => {
   socket.emit("leaveRoom");
@@ -63,10 +92,86 @@ const gameStartHandler = (socket) => {
   socket.emit("gameStart", universeName.split("=")[1]);
 };
 
-const cleanScreen = () => {
-  document.querySelector("body").innerHTML = "";
-};
+document.addEventListener('mousemove', (event) => {
+  // Calculate changes in mouse position
+  const deltaX = event.movementX;
+  const deltaY = event.movementY;
 
+  // Adjust yaw and pitch based on mouse movement
+  yaw -= deltaX * mouseSensitivity;
+
+  // Reduce the vertical sensitivity to prevent excessive vertical movement
+  pitch -= deltaY * (mouseSensitivity / 2); // Lower sensitivity for pitch
+
+  // Clamp pitch to stay within ±30 degrees
+  pitch = Math.max(-pitchLimit, Math.min(pitchLimit, pitch));
+
+  // Update camera rotation
+  camera.rotation.set(pitch, yaw, 0);
+}, false);
+
+document.addEventListener("keyup", (event) => {
+ switch (event.key) {
+   case "ArrowUp":
+   case "w":
+     moveForward = false;
+     break;
+   case "ArrowDown":
+   case "s":
+     moveBackward = false;
+     break;
+   case "ArrowLeft":
+   case "a":
+     moveLeft = false;
+     break;
+   case "ArrowRight":
+   case "d":
+     moveRight = false;
+     break;
+ }
+});
+
+document.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "ArrowUp":
+    case "w":
+      moveForward = true;
+      break;
+    case "ArrowDown":
+    case "s":
+      moveBackward = true;
+      break;
+    case "ArrowLeft":
+    case "a":
+      moveLeft = true;
+      break;
+    case "ArrowRight":
+    case "d":
+      moveRight = true;
+      break;
+    case " ":
+      if (!isJumping) {
+        isJumping = true;
+        jumpVelocity = jumpSpeed;
+      }
+      break;
+  }
+});
+
+
+leaveBtn.addEventListener("click", () => leaveRoomHandler(socket));
+title.addEventListener("click", copyRoomNameHandler);
+startBtn.addEventListener("click", () => {
+  gameStartHandler(socket);
+});
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+
+/** ------------------- 소켓 함수 ------------------------------ */
 socket = io("http://localhost:3000");
 universeName = window.location.search.slice(1);
 roomId = universeName.split("=")[1];
@@ -80,16 +185,13 @@ if (universeName.length === 0) {
 
 socket.emit("joinRoom", roomId);
 socket.on("joinedRoom", (data) => {
-  changeRoomInit(data.id);
-  changeJoinedPlayer(data.players);
+  changeRoomData(data);
 });
 
 socket.on("startedGame", (data) => {
   alert(data.message);
   socket.emit("gameInit", roomId);
-  cleanScreen();
-  initThreeJS();
-  initGameScreen();
+  init();
 });
 
 socket.on("error", (message) => {
@@ -99,18 +201,12 @@ socket.on("error", (message) => {
 
 socket.on("playerJoined", (data) => {
   console.log(`Player joined: ${data.id}`);
-  changeJoinedPlayer(data.players);
+  changeRoomData({id: roomId, players:data.players})
 });
 
 socket.on("playerDisconnected", (data) => {
   console.log(`Player disconnected: ${data.id}`);
-  changeJoinedPlayer(data.players);
-});
-
-leaveBtn.addEventListener("click", () => leaveRoomHandler(socket));
-title.addEventListener("click", copyRoomNameHandler);
-startBtn.addEventListener("click", () => {
-  gameStartHandler(socket);
+  changeRoomData({id: roomId, players:data.players})
 });
 
 socket.on("initPlayer", (data) => {
@@ -147,71 +243,6 @@ function createOtherPlayer(id, camp, position = { x: 0, y: 0, z: 0 }) {
   otherPlayers[id] = otherPlayer;
 }
 
-document.addEventListener("keydown", (event) => {
-  switch (event.key) {
-    case "ArrowUp":
-    case "w":
-      moveForward = true;
-      break;
-    case "ArrowDown":
-    case "s":
-      moveBackward = true;
-      break;
-    case "ArrowLeft":
-    case "a":
-      moveLeft = true;
-      break;
-    case "ArrowRight":
-    case "d":
-      moveRight = true;
-      break;
-    case " ":
-      if (!isJumping) {
-        isJumping = true;
-        jumpVelocity = jumpSpeed;
-      }
-      break;
-  }
-});
-
-document.addEventListener('mousemove', (event) => {
-   // Calculate changes in mouse position
-   const deltaX = event.movementX;
-   const deltaY = event.movementY;
- 
-   // Adjust yaw and pitch based on mouse movement
-   yaw -= deltaX * mouseSensitivity;
- 
-   // Reduce the vertical sensitivity to prevent excessive vertical movement
-   pitch -= deltaY * (mouseSensitivity / 2); // Lower sensitivity for pitch
- 
-   // Clamp pitch to stay within ±30 degrees
-   pitch = Math.max(-pitchLimit, Math.min(pitchLimit, pitch));
- 
-   // Update camera rotation
-   camera.rotation.set(pitch, yaw, 0);
-}, false);
-
-document.addEventListener("keyup", (event) => {
-  switch (event.key) {
-    case "ArrowUp":
-    case "w":
-      moveForward = false;
-      break;
-    case "ArrowDown":
-    case "s":
-      moveBackward = false;
-      break;
-    case "ArrowLeft":
-    case "a":
-      moveLeft = false;
-      break;
-    case "ArrowRight":
-    case "d":
-      moveRight = false;
-      break;
-  }
-});
 
 function update() {
   const moveDirection = new THREE.Vector3();
@@ -269,40 +300,3 @@ function animate() {
   update();
   renderer.render(scene, camera);
 }
-
-function initGameScreen() {
-  console.log("Initializing game screen...");
-
-  // Create player
-  const playerGeometry = new THREE.BoxGeometry(1, 1, 1);
-  const playerMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  player = new THREE.Mesh(playerGeometry, playerMaterial);
-  scene.add(player);
-
-  // Create floor
-  const floorGeometry = new THREE.PlaneGeometry(100, 100);
-  const floorMaterial = new THREE.MeshBasicMaterial({ color: 0xaaaaaa, side: THREE.DoubleSide });
-  floor = new THREE.Mesh(floorGeometry, floorMaterial);
-  floor.rotation.x = -Math.PI / 2;
-  floor.position.y = floorY;
-  scene.add(floor);
-
-  // Set camera position and add to player
-  camera.position.set(0, 0.9, 0);
-  player.position.set(0, 0, 0);
-  player.rotation.set(0, 0, 0);
-  player.add(camera);
-
-  // Add a light source
-  const light = new THREE.PointLight(0xffffff, 1, 100);
-  light.position.set(10, 10, 10);
-  scene.add(light);
-
-  animate();
-}
-
-window.addEventListener("resize", () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-});
